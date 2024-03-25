d7b338af14a851ad4b2f218fd8f51632e537fb29
Habib-bhai <habibahmed918131@gmail.com>
2024-03-25 10:55:41 -0700

    first commit

---
+++ app.ts
@@ -0,0 +1,23 @@
+#! usr/bin/env node
+import inquirer from "inquirer"
+//await means to wait untill the operation after that "await" is being done.
+const answer = await inquirer.prompt([{ message: "enter first number", type: "number", name: "firstNumber" }, 
+{ message: "Enter second number", type: "number", name: "secondNumber" }, 
+{ message: "select one of the operator to perform operation", type: "list", name: "operator", choices: ["addition","subtraction","multiplication","division"] }])
+
+if(answer.operator === "addition"){
+    console.log(`your value is ${answer.firstNumber + answer.secondNumber}`)
+}
+else if(answer.operator === "subtraction"){
+    console.log(`your value is ${answer.firstNumber - answer.secondNumber}`)
+}
+else if(answer.operator === "multiplication"){
+    console.log(`your value is ${answer.firstNumber * answer.secondNumber}`)
+}
+else if(answer.operator === "division"){
+    console.log(`your value is ${answer.firstNumber / answer.secondNumber}`)
+}
+else {
+    console.log("you have entered a wrong operator")
+}
+
